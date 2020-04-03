7f046d85692a0937f0f6a9340f1b0b3e2cbf0c00
reed-lau <geoliuwei@gmail.com>
2020-04-03 22:00:21 +0800

    init

---
+++ popup.js
@@ -0,0 +1,7 @@
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
+
+});
